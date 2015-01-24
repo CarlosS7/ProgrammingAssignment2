ae6ae0f11c13d09f9b515fbb17db3c132e2b02dc
CarlosS7 <carlosalas7@gmail.com>
2015-01-23 23:11:19 -0600

    Adding the modified version of cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In this two functions, the first one calculates the inverse of a matrix  
+## and then the second one if it is needed again, it gets it from the cache
 
-## Write a short comment describing this function
+## This function calculates the inverse of the matrix x
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        tam <- NULL
+        inv <- function (y = matrix()) {
+                x <<- y
+                tam <<- NULL
+        }
+        mat <- function() x
+        matinv <- function (solve) tam <<- solve
+        getinv <- function () tam
+        list(inv = inv, mat = mat, matinv = matinv, getinv = getinv )
 }
 
 
-## Write a short comment describing this function
+## The following function, looks for the inverse of the matrix in the cache and if 
+## it is empty, it calculates it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        tam <- x$getinv()
+        if(!is.null(tam)){
+                message("getting cached data")
+                return(tam)
+        }
+        data <- x$mat()
+        matinv <- solve(data, ...)
+        matinv
 }
+
